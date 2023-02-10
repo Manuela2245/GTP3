631f8d2e7d665a7371464b5f851bfad56777223f
Manuela2245 <malvarezc1@eafit.edu.co>
2023-02-10 09:00:15 -0500

    first commit

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./feature.css"
+
+const Feature = ({title,text}) => {
+  return (
+    <div className='app1__feature-container__feature'>
+      <div className='app1__feature-container__feature-title'>
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className='app1__feature-container__feature-text'>
+        {text}
+      </div>
+    </div>
+  )
+}
+
+export default Feature
